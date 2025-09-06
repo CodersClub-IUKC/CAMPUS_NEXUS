9d9724583bb68da4199e7d0bd58617fccf56c055
jaidesx <ssalijamal697@gmail.com>
2025-09-06 17:39:47 +0300

    Customized jazzmin interface.

--- .nexusenv/bin/activate.fish
+++ .nexusenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/jamie-fox/Desktop/CAMPUS_NEXUS/.nexusenv
+set -gx VIRTUAL_ENV /home/jamie-foxx/Desktop/CAMPUS_NEXUS/.nexusenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
